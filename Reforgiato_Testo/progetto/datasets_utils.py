7c8bc14b5dbe2a5d155e09bad2eda126b5833a54
JacopoMereu <jachi334@gmail.com>
2023-04-04 17:13:46 +0200

    Pushed the vegetables dataset. Made the get_vegetables_loaders

--- Reforgiato_Testo/progetto/datasets_utils.py
+++ Reforgiato_Testo/progetto/datasets_utils.py
@@ -132,3 +132,68 @@ def get_Chaoyang_loaders(BATCH_SIZE = 16, SEED=42, SPLITS=[0.8,0.1,0.1]):
 
     return train_dataloader, val_dataloader, test_dataloader, classes, 224
     
+
+def get_vegetables_dataloader(BATCH_SIZE = 16, SEED=42, SPLITS=[0.8,0.1,0.1]):
+    PATH = './vegetables/'
+    MY_TRANSFORMATIONS = A.Compose([
+        A.Resize(256,256),
+        ToTensorV2()
+        ])
+    
+    class VegetableDataset(Dataset):
+        def __init__(self,DF, TRANSFORMATIONS):
+            self._df = DF
+            self._transformations = TRANSFORMATIONS
+
+            self.images = self._df['path'].values
+            self.classes = self._df['class'].values
+            self.classes = np.array([classes.index(i) for i in self.classes])
+            
+        def __len__(self):
+            return len(self._df)
+        
+        def __getitem__(self,idx):
+            image = self.images[idx]
+            image = plt.imread(image)
+            #image = np.transpose(image,(2,0,1))
+            #if is grayscale, convert to rgb
+            if image.shape[0] == 1:
+                image = np.repeat(image,3,0)
+            #cast to float and normalize
+            image = image.astype(np.float32)
+            image = image/255.0 
+            image =  self._transformations(image=image)['image']
+            class_ = self.classes[idx]
+            class_ = torch.tensor(class_,dtype=torch.long)
+            image = image.type(torch.FloatTensor)
+            if image.shape[0] == 1:
+                image = torch.repeat_interleave(image,3,0)
+            return image,class_
+        
+    #read all files from the folder CUB_200_2011 and assign the subfolder as a class
+    #the subfolder name is the class name
+    classes = os.listdir(PATH)
+    classes.sort()
+    print(classes)
+
+    #read all files from the subfolders
+    data = []
+    for i in range(len(classes)):
+        folder = os.path.join(PATH,classes[i])
+        files = os.listdir(folder)
+        for j in range(len(files)):
+            data.append([classes[i],os.path.join(folder,files[j])])
+
+    #convert the list to a dataframe
+    df = pd.DataFrame(data,columns=['class','path'])
+    df.head()
+
+    #split the data into train and test and validation
+    train_df, val_df, test_df = get_train_valid_test_split(df, SPLITS=SPLITS, SEED=SEED)
+    
+    #create the dataloaders
+    train_loader = DataLoader(VegetableDataset(train_df, MY_TRANSFORMATIONS),batch_size=BATCH_SIZE,shuffle=True)
+    val_loader = DataLoader(VegetableDataset(val_df, MY_TRANSFORMATIONS),batch_size=BATCH_SIZE,shuffle=False)
+    test_loader = DataLoader(VegetableDataset(test_df, MY_TRANSFORMATIONS),batch_size=BATCH_SIZE,shuffle=False)
+
+    return train_loader, val_loader, test_loader, classes, 256
